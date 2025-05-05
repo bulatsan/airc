ed0b1e01be7a5522521afadc9eec9c2743e26305
bulat <bulat.akhmanov@gmail.com>
2025-05-05 15:11:36 +0300

    fixed tmp dir location

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
-: "${AIRC_SRC:=./cmd/app/main.go}"
-: "${AIRC_BIN:=/tmp/app}"
-: "${AIRC_CMD:=go build -o $AIRC_BIN $AIRC_SRC}"
+: "${AIRC_ROOT:=.}"
+: "${AIRC_TMP_DIR:=/tmp}"
+: "${AIRC_SRC:=$AIRC_ROOT/cmd/app/main.go}"
+: "${AIRC_BIN:=$AIRC_TMP_DIR/app}"
+: "${AIRC_CMD:=go build -modfile $AIRC_ROOT/go.mod -o $AIRC_BIN $AIRC_SRC}"
 : "${AIRC_DELAY:=1000}"
 
 exec air \
-  -build.bin               "$AIRC_BIN" \
-  -build.cmd               "$AIRC_CMD" \
-  -build.delay             "$AIRC_DELAY" \
-  -build.send_interrupt    "true" \
-  -log.silent              "true" \
+  -root                 "$AIRC_ROOT" \
+  -tmp_dir              "$AIRC_TMP_DIR" \
+  -build.bin            "$AIRC_BIN" \
+  -build.cmd            "$AIRC_CMD" \
+  -build.delay          "$AIRC_DELAY" \
+  -build.send_interrupt "true" \
+  -log.silent           "true" \
   "$@"
